e84b05954f53c3890c4b3f069116a9ace1464bcb
Mehdi Mulani <mars.martian@gmail.com>
2020-01-20 05:37:34 +0000

    Print title.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
 use std::env;
 use std::fs;
 
+mod gba {
+    use std::fs;
+
+    pub struct ROM {
+        content: Vec<u8>,
+    }
+
+    impl ROM {
+        pub fn from_path(filename: &str) -> ROM {
+            ROM {
+                content: match fs::read(filename) {
+                    Ok(bytes) => bytes,
+                    Err(_e) => vec![],
+                }
+            }
+        }
+
+        pub fn title(&self) -> String {
+            let title: Vec<u8> = self.content[0x134..0x144]
+                .iter()
+                .take_while(|&&v| v > 0)
+                .cloned()
+                .collect();
+            let str = String::from_utf8(title).unwrap();
+            str
+        }
+    }
+}
+
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
@@ -10,9 +39,8 @@ fn main() -> Result<(), std::io::Error> {
 
     println!("Hello, world! {}", bytes.len());
 
-    for i in 0x104..0x133 {
-        println!("Byte at {:#x}:{:#04x}", i, bytes[i]);
-    }
+    let rom = gba::ROM::from_path(filename);
+    println!("rom title {}", rom.title());
 
     Ok(())
 }
