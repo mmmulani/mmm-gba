3b29370f055113d3a31843b3754360f18b980092
Mehdi Mulani <mars.martian@gmail.com>
2020-01-20 03:34:04 +0000

    Basic outputting of ROM info.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    println!("Got filename {}", filename);
+    let bytes: Vec<u8> = fs::read(filename)?;
+
+    println!("Hello, world! {}", bytes.len());
+
+    for i in 0x104..0x133 {
+        println!("Byte at {:#x}:{:#04x}", i, bytes[i]);
+    }
+
+    Ok(())
 }
